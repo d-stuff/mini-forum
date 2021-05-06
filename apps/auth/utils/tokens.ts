cc9bb45ecb55365e23763299ae3d8d35d9cb9f7f
Vlad Furman <deadrop308@gmail.com>
2021-05-06 19:34:18 +0300

    fixed jwt import

--- apps/auth/utils/tokens.ts
+++ apps/auth/utils/tokens.ts
@@ -1,14 +1,14 @@
-import jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import { COOKIE_SECRET } from '../config';
 
 
 
 
 export const signToken = (data) => {
-    return jwt.sign({ data }, COOKIE_SECRET, { expiresIn: '30d' })
+    return jwt.sign( {data}, COOKIE_SECRET, { expiresIn: '30d' })
 }
 
 
 export const verifyToken = (token) => {
-    return jwt.verify( token , COOKIE_SECRET )
+    return jwt.verify( token , COOKIE_SECRET ).data;
 }
