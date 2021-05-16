efaf7e57b7a74b1c747ba73eee3a8f42b7f6cde4
Jacob <jacobmarkmanmusic@gmail.com>
2021-05-16 15:22:16 +0300

    added first styling2

--- ui/src/state/configure.store.js
+++ ui/src/state/configure.store.js
@@ -1,11 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import post from "./post.store";
 
 
 import logger from "../middleware/logger.middleware";
 
 const store = configureStore({
   reducer: {
-    // count: countReducer
+    post: postReducer
   },
   devTools: process.env.NODE_ENV !== "production",
   middleware: [...getDefaultMiddleware(), logger]
