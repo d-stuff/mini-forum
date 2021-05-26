21f9983452a9a9981dc3e6430e6aa13f1ca87192
Jacob <jacobmarkmanmusic@gmail.com>
2021-05-26 18:30:13 +0300

    added static files serving

--- apps/gateway/index.ts
+++ apps/gateway/index.ts
@@ -1,6 +1,8 @@
 import {app} from 'api-server';
 import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 import fetch from 'node-fetch';
+import * as express from 'express';
+import * as path from 'path';
 
 
 app.use(['/api/comments', '/api/posts'], async function checkAuth(req, res, next) {
@@ -16,7 +18,9 @@ app.use(['/api/comments', '/api/posts'], async function checkAuth(req, res, next
 app.use('/api/comments', createProxyMiddleware({ target: 'http://localhost:4002', changeOrigin: true }));
 app.use('/api/posts', createProxyMiddleware({ target: 'http://localhost:4001', changeOrigin: true }));
 app.use(['/api/users', '/api/me', '/api/register', '/api/change-password', '/api/login', '/api/logout'] , createProxyMiddleware({ target: 'http://localhost:4000', changeOrigin: true }));
-
-
+app.use(express.static('../ui/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../ui/build/index.html'));
+  })
 
 app.listen(process.env.PORT || 8080, () => console.log('Gateway app is running!'))
--- apps/ui/src/view/App.view.jsx
+++ apps/ui/src/view/App.view.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import Post from "./Post.view";
+import {useRoutes} from 'hookrouter';
+
+
+const routes = {
+    '/' :()=><Home/>,
+    '/post/:postId' :(postId)=> <Post id={postId}/>,
+   };
 
 
 const App = () => {
+    const routeResult = useRoutes(routes);
+
     return (
         <Box>
-            <Post />
+            {routeResult}
         </Box>
     );
 };
